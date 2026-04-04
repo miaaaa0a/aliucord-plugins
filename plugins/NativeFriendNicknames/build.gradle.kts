19ea25bc145d9b8e988aaf1f5f4961374022d343
miaaaa0a <miaaaa0a@proton.me>
2026-04-04 23:49:22 +0300

    plugins(NativeFriendNicknames): bump version

--- plugins/NativeFriendNicknames/build.gradle.kts
+++ plugins/NativeFriendNicknames/build.gradle.kts
@@ -1,15 +1,15 @@
-version = "1.1.0"
-description = "[wip] a plugin that backports the friend nickname feature"
+version = "1.1.1"
+description = "a plugin that backports the friend nickname feature"
 
 aliucord {
     // Changelog of your plugin
     changelog.set(
         """
-        # 1.1.0
-        * implement editing of the nicknames
+        # 1.1.1
+        * minor fixes
         """.trimIndent(),
     )
-    deploy.set(false)
+    deploy.set(true)
 
     // Builds and deploys this plugin but excludes it from global plugin repositories.
     // Set this if the plugin has reached EOL but a last update should still occur.
